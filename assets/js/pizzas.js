30c31eedc9f04b1340effaa298d9463a871d9974
c-diego <diegocorte02@hotmail.com>
2023-07-16 16:45:24 -0300

    template do projeto

---
+++ assets/js/pizzas.js
@@ -0,0 +1,9 @@
+const pizzaJson = [
+  { id: 1, name: 'Mussarela', img: 'assets/images/pizza.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 2, name: 'Calabresa', img: 'assets/images/pizza.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 3, name: 'Quatro Queijos', img: 'assets/images/pizza.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 4, name: 'Americana', img: 'assets/images/pizza.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 5, name: 'Tomate', img: 'assets/images/pizza.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 6, name: 'Moda da Casa', img: 'assets/images/pizza.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 7, name: 'Frango', img: 'assets/images/pizza.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' }
+];
