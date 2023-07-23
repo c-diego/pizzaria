4bf10a98e9fdef324ecd3d8d0b6f821076b4bf51
c-diego <diegocorte02@hotmail.com>
2023-07-23 15:35:19 -0300

    Subindo as imagens em um servidor diferente

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Pizzaria 🍕
 Este projeto incrível foi desenvolvido durante o curso de JavaScript da B7Web. Trata-se de uma emocionante simulação de um site voltado para pizzarias, criado com dedicação e paixão pela arte da programação.
 
-![Página inicial do site!](/assets/images/site.png "Home")
+![Página inicial do site!](https://images2.imgbox.com/24/d8/55nuDPcG_o.png "Home")
--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -1,9 +1,9 @@
 const pizzaJson = [
-  { id: 1, name: 'Mussarela', img: 'assets/images/pizza.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 2, name: 'Calabresa', img: 'assets/images/pizza.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 3, name: 'Quatro Queijos', img: 'assets/images/pizza.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 4, name: 'Americana', img: 'assets/images/pizza.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 5, name: 'Tomate', img: 'assets/images/pizza.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 6, name: 'Moda da Casa', img: 'assets/images/pizza.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
-  { id: 7, name: 'Frango', img: 'assets/images/pizza.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' }
+  { id: 1, name: 'Cinco Queijos', img: 'https://images2.imgbox.com/5e/c7/G3JCIDkN_o.png', price: 20.19, sizes: ['100g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 2, name: 'Mussarela', img: 'https://images2.imgbox.com/c3/70/g8tiCF5J_o.png', price: 18.00, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 3, name: 'Quatro Queijos', img: 'https://images2.imgbox.com/de/0c/6DPvo49G_o.png', price: 17.45, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 4, name: 'Americana', img: 'https://images2.imgbox.com/f7/3b/E63raGbw_o.png', price: 19.77, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 5, name: 'Tomate', img: 'https://images2.imgbox.com/7e/1f/qB387JyV_o.png', price: 21.43, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 6, name: 'Moda da Casa', img: 'https://images2.imgbox.com/ff/bb/58l5f5NC_o.png', price: 18.55, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' },
+  { id: 7, name: 'Chocolate', img: 'https://images2.imgbox.com/ad/98/6UiTL5mf_o.png', price: 22.36, sizes: ['320g', '530g', '860g'], description: 'Descrição da pizza em mais de uma linha muito legal bem interessante' }
 ];
